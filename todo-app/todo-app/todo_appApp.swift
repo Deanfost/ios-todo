2e75ef041e9eac41a758241a8c8ec47d8315c610
Dean Foster <Deanfoster11@gmail.com>
2024-12-07 20:34:35 -0500

    Create project

---
+++ todo-app/todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Dean Foster on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
